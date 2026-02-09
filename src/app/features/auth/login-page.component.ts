716e7530003a26ace1ac51495047872f56de14c7
salma hamdi <saalmahm51@gmail.com>
2026-02-09 11:59:22 +0100

    feat: créer les composants login, register et profil

---
+++ src/app/features/auth/login-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-home-page',
+  template: `<h1>login</h1>`,
+})
+export class LoginPageComponent {}
---
+++ src/app/features/auth/register-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-home-page',
+  template: `<h1>JobFinder - signup</h1>`,
+})
+export class RegisterPageComponent {}
